ee150286e3d24f07d1041b7dfe3c660c3a6521ba
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2023-08-26 18:48:50 -0300

    front basico hecho, css en /public/css, html en /templates/taskViewer.ejs

--- api/routes/root.js
+++ api/routes/root.js
@@ -4,25 +4,7 @@ import { pool } from '../dbconnection.js'
 export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     const query = await pool.query('SELECT * FROM Tasks');
-    const res = await query.rows;
-    return res;
+    const tasks = await query.rows;
+    return reply.view("templates/taskViewer.ejs", { tasks })
   })
 }
-
-/*
-Todo esto debería ser delegado a un controller:
-
-async function (request, reply) {
-    const query = await pool.query('SELECT * FROM Tasks');
-    const res = await query.rows;
-    return res;
-  })
-
-Quedando así el código de la ruta:
-
-import { getResource } from '../controllers/resourceController.js';
-
-export default async function (fastify, opts) {
-  fastify.get('/', getResource(request, reply))
-}
-*/
