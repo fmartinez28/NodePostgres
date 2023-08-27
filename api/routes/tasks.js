30f62df133da645358b30b16de9a556ba8b425be
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2023-08-27 12:05:12 -0300

    hecho el POST, DELETE y PUT de la api

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,12 +2,34 @@ import { pool } from '../dbconnection.js'
 
 //A modo de ejemplo, un get
 export default async function (fastify, opts) {
-  fastify.get('/tasks', async function (request, reply) {
-    const query = await pool.query('SELECT * FROM Tasks');
-    const res = await query.rows;
-    return res;
-  })
+    fastify.get('/tasks', async function (request, reply) {
+        const query = await pool.query('SELECT * FROM Tasks;');
+        const res = await query.rows;
+        return res;
+    });
+    fastify.post('/tasks', async function (request, reply) {
+        const name = request.body.name;
+        const description = request.body.description;
+        const query = await pool.query(`INSERT INTO Tasks ("name", "description") values ('${name}', '${description}');`);
+        const res = await query.rows;
+        return res;
+    });
+    fastify.delete('/tasks/:id', async function (request, reply) {
+        const { id } = request.params;
+        const query = await pool.query(`DELETE from Tasks where id = ${id};`);
+        const res = await query.rows;
+        return res;
+    });
+    fastify.put('/tasks/:id', async function (request, reply) {
+        const { id } = request.params;
+        const name = request.body.name;
+        const description = request.body.description;
+        const query = await pool.query(`update Tasks set name = '${name}', description = '${description}' where id = ${id};`)
+        const res = await query.rows;
+        return res;
+    })
 }
+
 
 /*
 Todo esto debería ser delegado a un controller:
